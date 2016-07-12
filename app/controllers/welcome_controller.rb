35512be0a1355d6509d4d2cd089547d795d6a7ac
Kevin Bourne <granddrive@live.com>
2016-07-12 10:18:56 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homestate = "Georgia"
+  	@countries = ["Jamaica", "Canada", "England", "USA"]
+
+  	@pictures = ["img-test.jpg", "img2.jpg", "img3.jpg", "img4.jpg"]
+  	@pictures_hash = {"img-test.jpg" => "blue mountains", "img2.jpg" => "vancouver", 
+  		"img3.jpg" => "england", "img4.jpg" => "USA"}
+  end
+
+  def about
+		@color = params[:color]
+		@size = params[:size].to_f
+  end
+end
